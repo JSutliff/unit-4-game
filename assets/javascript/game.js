27fb95845fe8923a72c4e8d321d3877c2ec29ef3
Jason Sutliff <jbopsut@gmail.com>
2018-08-06 15:25:39 -0700

    crystal values adding to score on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,9 +15,13 @@ function initializeGame() {
 compScore = getRandomArbitrary(19, 120);
 userScore = 0;
 crystal1 = getRandomArbitrary(1, 12);
+$('#crystal-1').attr('value', crystal1);
 crystal2 = getRandomArbitrary(1, 12);
+$('#crystal-2').attr('value', crystal2);
 crystal3 = getRandomArbitrary(1, 12);
+$('#crystal-3').attr('value', crystal3);
 crystal4 = getRandomArbitrary(1, 12);
+$('#crystal-4').attr('value', crystal4);
 }
 
 function updateDom() {
@@ -27,3 +31,27 @@ function updateDom() {
 
 initializeGame();
 updateDom();
+
+$('#crystal-1').on('click', function() {
+  var num = parseInt($(this).attr('value'));
+  userScore += num;
+  updateDom();
+});
+
+$('#crystal-2').on('click', function() {
+  var num = parseInt($(this).attr('value'));
+  userScore += num;
+  updateDom();
+});
+
+$('#crystal-3').on('click', function() {
+  var num = parseInt($(this).attr('value'));
+  userScore += num;
+  updateDom();
+});
+
+$('#crystal-4').on('click', function() {
+  var num = parseInt($(this).attr('value'));
+  userScore += num;
+  updateDom();
+});
