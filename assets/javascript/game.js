c841123f283f7a7059e6f828e77bc8b3ec06f1df
Jason Sutliff <jbopsut@gmail.com>
2018-08-06 12:55:48 -0700

    initialize game function created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,29 @@
-var compScore = getRandomArbitrary(19, 120);
+var compScore;
 var userScore;
 var wins = 0;
 var losses = 0;
-var crystal1 = getRandomArbitrary(1, 12);
-var crystal2 = getRandomArbitrary(1, 12);
-var crystal3 = getRandomArbitrary(1, 12);
-var crystal4 = getRandomArbitrary(1, 12);
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
 
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function updateDom() {
-  $('#compScore').text(compScore);
-  
+function initializeGame() {
+compScore = getRandomArbitrary(19, 120);
+userScore = 0;
+crystal1 = getRandomArbitrary(1, 12);
+crystal2 = getRandomArbitrary(1, 12);
+crystal3 = getRandomArbitrary(1, 12);
+crystal4 = getRandomArbitrary(1, 12);
 }
 
+function updateDom() {
+  $('#compScore').text(compScore);
+  $('#userScore').text(userScore);
+}
 
+initializeGame();
 updateDom();
