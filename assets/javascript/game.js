81971841b6afe52512eceb43875a3ac218c9f6ce
Jason Sutliff <jbopsut@gmail.com>
2018-08-06 11:52:15 -0700

    variables initialized and randomNum funciton created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,20 @@
-console.log('test');
+var compScore = getRandomArbitrary(19, 120);
+var userScore;
+var wins = 0;
+var losses = 0;
+var crystal1 = getRandomArbitrary(1, 12);
+var crystal2 = getRandomArbitrary(1, 12);
+var crystal3 = getRandomArbitrary(1, 12);
+var crystal4 = getRandomArbitrary(1, 12);
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function updateDom() {
+  $('#compScore').text(compScore);
+  
+}
+
+
+updateDom();
